47cd70321e7b595feb3c77938bbbfd8243660a6a
Josue Labres <josue.labres.cruz@gmail.com>
2021-11-03 17:48:33 -0300

    urls

--- radarCrawler/spiders/radar.py
+++ radarCrawler/spiders/radar.py
@@ -1,13 +1,13 @@
 import scrapy
 import csv
+import requests
 
 class RadarSpider(scrapy.Spider):
     name = 'radar'
     start_urls = []
-    arquivo_aberto = open('./urls.csv', 'r') 
-    urls =  csv.reader(arquivo_aberto,delimiter=',')
-    for row in urls:
-        start_urls.append(row[0])
+    r = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vSgpi7PovoovcZL28XpERljhBfOR_DFLlGNzwvlr2DyDn8-sBw9qvpfeh954TOm0eDZqBU_r8O0Bp5o/pub?gid=0&single=true&output=csv').text
+    start_urls = r.split('\n')
+    
 
     def parse(self, response):
         yield {
