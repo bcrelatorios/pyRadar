260536b9da8ceffc7fdfb0de53224db477ed2c49
Josue Labres <josue.labres.cruz@gmail.com>
2021-11-03 17:32:24 -0300

    diretorio

--- radarCrawler/spiders/radar.py
+++ radarCrawler/spiders/radar.py
@@ -4,7 +4,7 @@ import csv
 class RadarSpider(scrapy.Spider):
     name = 'radar'
     start_urls = []
-    arquivo_aberto = open('urls.csv', 'r') 
+    arquivo_aberto = open('/urls.csv', 'r') 
     urls =  csv.reader(arquivo_aberto,delimiter=',')
     for row in urls:
         start_urls.append(row[0])
