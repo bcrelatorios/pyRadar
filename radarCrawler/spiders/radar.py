851cc83ab0b6e750713a4d9839d5279fc7759069
Josue Labres <josue.labres.cruz@gmail.com>
2021-11-03 17:27:06 -0300

    mais urls

--- radarCrawler/spiders/radar.py
+++ radarCrawler/spiders/radar.py
@@ -1,9 +1,13 @@
 import scrapy
-
+import csv
 
 class RadarSpider(scrapy.Spider):
     name = 'radar'
-    start_urls = ['https://www.zumgrafica.com.br']
+    start_urls = []
+    arquivo_aberto = open('urls.csv', 'r') 
+    urls =  csv.reader(arquivo_aberto,delimiter=',')
+    for row in urls:
+        start_urls.append(row[0])
 
     def parse(self, response):
         yield {
